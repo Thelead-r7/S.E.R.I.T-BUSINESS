c56d04ce8992d216b4cc316bf86ecc6022aa754d
Thelead-r7 <estudiantinemood@gmail.com>
2025-07-27 21:37:23 +0000

    Update admin.js

--- admin.js
+++ admin.js
@@ -31,12 +31,12 @@ import {
 // --- Config Firebase ---
 // Remplace par ta config Firebase
 const firebaseConfig = {
-  apiKey: "TA_CLE_API",
-  authDomain: "TON_PROJET.firebaseapp.com",
-  projectId: "TON_PROJET",
-  storageBucket: "TON_PROJET.appspot.com",
-  messagingSenderId: "XXXXXXXXXXX",
-  appId: "1:XXXXXXXXX:web:XXXXXXXXX",
+apiKey: "AIzaSyDPavrp_mfV0POiDF3V4ZOZ0SHZGwgTmGs",
+  authDomain: "serit-societe.firebaseapp.com",
+  projectId: "serit-societe",
+  storageBucket: "serit-societe.firebasestorage.app",
+  messagingSenderId: "25581120285",
+  appId: "1:25581120285:web:b8ee391fabf3d626480ff4"
 };
 
 const app = initializeApp(firebaseConfig);
